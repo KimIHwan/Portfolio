42c1d175ff39b2335c54bb04d6412ea9866d4d14
KimIHwan <lionf2000@naver.com>
2024-10-13 23:50:38 +0900

    리치 마작추가

--- src/Components/Project/PCard.jsx
+++ src/Components/Project/PCard.jsx
@@ -4,7 +4,7 @@ import './PCard.css'
 import ReactCardFlip from 'react-card-flip'
 import { BsFillArrowLeftSquareFill } from "react-icons/bs"
 import { SiVercel } from "react-icons/si";
-
+import { TiCancel } from "react-icons/ti";
 
 const PCard = ({proj}) => {
     const [isFlipped, setIsFlipped] = useState(false)
@@ -21,23 +21,34 @@ const PCard = ({proj}) => {
                 <button className='more' onClick={flipCard}>상세 보기</button>
             </div>
             <div className='Pcard card-back'>
-                <button className='back'onClick={flipCard}><BsFillArrowLeftSquareFill className='b'/></button>
-                <pre>{proj.info}</pre>
-                <div className='cLink'>  
-                    {proj.code ? 
-                        <div className="f-icon">
-                            소스코드 : <a href={proj.code}><Github color='white' size='2rem'/></a>
-                        </div>
-                            :null 
-                            // 깃허브 링크가 없을 경우
-                    }
-                    {proj.url ?
-                        <div className="f-icon">
-                            배포 : <a href={proj.url}><SiVercel color='white' size='2rem'/></a>
-                        </div>
-                            :null
-                    }
+                <div style={{height:"3rem", width:"100%"}}>
+                    <button className='back'onClick={flipCard}><BsFillArrowLeftSquareFill className='b' size='2rem'/></button>
                 </div>
+                <div style={{height:"16rem", width:"100%",}}>
+                    <pre>{proj.info}</pre>
+                </div>
+                <div style={{height:"3rem", width:"100%"}}>
+                    <div className='cLink'>  
+                        {proj.code ? 
+                            <div className="f-icon">
+                                소스코드 : <a href={proj.code}><Github color='white' size='2rem'/></a>
+                            </div>
+                                :null 
+                                // 깃허브 링크가 없을 경우
+                        }
+                        {proj.url ?
+                            proj.url === "x" ? 
+                            <div className="f-icon">
+                                배포 : <TiCancel color='white' size="2rem" /> 일시 중단
+                            </div>
+                            :<div className="f-icon">
+                                배포 : <a href={proj.url}><SiVercel color='white' size='2rem'/></a>
+                            </div>
+                                :null
+                        }
+                    </div>
+                </div>
+                
                 
             </div>
         </ReactCardFlip>        
--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -5,6 +5,7 @@ import GIRIN from '../../img/GIRIN.png'
 import ForestSeven from '../../img/ForestSeven.PNG'
 import Stick from '../../img/dongdongstick.PNG'
 import Portfolio from '../../img/portfolio.PNG'
+import MahjongCal from '../../img/mahjongCal.png'
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination, Navigation } from 'swiper/modules'
 import 'swiper/css/pagination'
@@ -17,6 +18,11 @@ const Project = () => {
   
   const clients =[
     {
+      img: MahjongCal,
+      review:"리치마작 계산기",
+      info: "구성요소 : React Native\n제작 기간 : 2024.02~\n\nReact Native로 제작한 리치마작 계산기 어플입니다.\n리치마작의 점수 계산을 쉽고 편하게 이용할 수 있습니다.\n심플하고 깔끔한 UI와 직접 만든 계산 알고리즘이 특징이며,\n현재 구글 플레이 출시를 위해 비공개 테스트중입니다."
+    },
+    {
       img: Eighteen,
       review: "Eighteen",
       code: "https://github.com/hwan-2/eighteen",
@@ -27,6 +33,7 @@ const Project = () => {
       img: GIRIN,
       review:"Game Information & Review In Node.js",
       code: "https://github.com/KimIHwan/GIRIN",
+      url:"x",
       info: "플레이 해본 게임의 리뷰가 작성 가능한 게임 리뷰 웹사이트 입니다.\n구성요소 : Node.js + EJS + MongoDB\n제작 기간: 2022.05~2022.06\n\n로그인 기능과 회원가입, 마이페이지, Admin 회원관리 기능이 있으며 게시글 CRUD도\n구현되어 있습니다. 게시글 별 카테고리가 나뉘어져 있으며, 검색기능도 구현하였습니다.\nDB는 MongoDB를 사용하였으며, DB에 계정과 게시글을 저장, 수정 및 삭제와 열람이\n가능합니다."
     },
     {
@@ -37,15 +44,16 @@ const Project = () => {
     },
     {
       img: Stick,
-      review:"RaspberryPi와 Python을 이용하여 만든 시각 장애인용 스마트 지팡이",
-      info: "구성요소 : Python\n사용한 툴 : Visual Studio Code + Pycharm + RaspberryPi\n제작 기간 : 2021.03~2021.11\n\n시각 장애인들을 위한 스마트 지팡이로, RaspberryPi의 센서제어를 이용하였습니다.\n제가 제작한 부분은 손잡이에 내장된 적외선 센서로 사용자가 지팡이를 놓쳤을 경우를\n판단하여 부저를 울리게 하는 기능과, 부저가 일정 시간 이상 울리면 사고 발생으로\n판단하여 기기에 장착된 GPS값을 읽어 등록된 보호자에게 SMS로 사용자의 위치를\n전송하는 기능입니다.\nSMS 전송에는 Naver Cloud Platform API를 사용하였습니다.\n또한 두 개의 초음파 센서를 각각 다른 높이에 설치하여 읽어낸 거리값의 차이를 이용하여\n계단일 경우 안내음이 울리는 기능도 구현하였습니다."
+      review:"시각 장애인용 스마트 지팡이",
+      info: "구성요소 : Python\n사용한 툴 : Visual Studio Code + Pycharm + RaspberryPi\n제작 기간 : 2021.03~2021.11\n\n시각 장애인들을 위한 스마트 지팡이로, RaspberryPi의 센서제어를 이용하였습니다.\n제가 제작한 부분은 손잡이에 내장된 적외선 센서로 사용자가 지팡이를 놓쳤을 경우를\n판단하여 부저를 울리게 하는 기능과, 부저가 일정 시간 이상 울리면 사고 발생으로\n판단하여 기기에 장착된 GPS값을 읽어 등록된 보호자에게 SMS로 사용자의 위치를\n전송하는 기능입니다.\nSMS 전송에는 Naver Cloud Platform API를 사용하였습니다.\n또한 두 개의 초음파 센서를 각각 다른 높이에 설치하여 읽어낸 거리값의 차이를\n이용하여 계단일 경우 안내음이 울리는 기능도 구현하였습니다."
     },
     {
       img: Portfolio,
       review:"김이환의 Portfolio",
       code: "https://github.com/KimIHWan/Portfolio",
       info: "구성요소 : React.js\n사용한 툴 : Visual Studio Code\n제작 기간 : 2023.10~\n\nReact로 제작한 개인 Portfolio 웹 사이트 입니다.\n개인적인 React 공부를 위해 React로 제작하였습니다.\n배포는 Github Page로 이뤄지고 있으며, 지속적으로 추가 및 수정해나가고 있습니다.\n소스 코드 열람 시 master branch로 확인 가능합니다."
-    }
+    },
+
   ]
   return (
     <div className='p-wrapper' id='Project'>
